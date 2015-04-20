7a1281f458117ea2a8aab15d16e9962e2e302ec9
kellysjohnson <KellyShimkoJohnson@Kellys-MacBook-Pro.local>
2015-04-20 18:56:16 -0500

    Pro Mode Works

--- prime_peer_jq_01.js
+++ prime_peer_jq_01.js
@@ -7,8 +7,13 @@ var i = 0;
 var age;
 var gender;
 var weight;
+var name;
 
-function Person(age, gender, weight){
+maleArray = ["Steve", "Joe", "Bill", "William", "Fred", "Kodi", "Harold", "Jack"];
+femaleArray = ["Samantha", "Wilhemina", "Mulan", "Jessica", "Susan", "Brenda", "Julie", "Alice"];
+
+
+function Person(age, gender, weight, name){
 	this.age = age;
 	this.gender = gender;
 	if (this.gender == 1){
@@ -16,22 +21,31 @@ function Person(age, gender, weight){
 	} else {
 		this.gender = "Female";
 	}
+	if (this.gender == "Male"){
+		this.name = maleArray[name];
+	} else {
+		this.name = femaleArray[name];
+	}
+
 	this.weight = weight;
+
 }
+
 
 
 $(document).ready(function() {
 	$('button').on("click", function(){
-
+		name = (randomNumber(1, maleArray.length) - 1);     //The flaw with array.length is that it ASSUMES the maleArray and femaleArray are the same length.
 		age = randomNumber(1,100);
 		gender = randomNumber(1,2);
 		weight = randomNumber(1,250);
 
-		var newPerson = new Person(age, gender, weight);
+		var newPerson = new Person(age, gender, weight, name);
 
-		$("#age").append("<tr><td id=" + i + ">" + newPerson.age + "</td></tr>");
-		$("#gender").append("<tr><td id=" + i + ">" + newPerson.gender + "</td></tr>");
-		$("#weight").append("<tr><td id=" + i + ">" + newPerson.weight + "</td></tr>");
+		$("#age").append("<tr><td>" + newPerson.age + "</td></tr>");
+		$("#gender").append("<tr><td>" + newPerson.gender + "</td></tr>");
+		$("#weight").append("<tr><td>" + newPerson.weight + "</td></tr>");
+		$("#name").append("<tr><td>" + newPerson.name + "</td></tr>");
 
 		i++;
 
